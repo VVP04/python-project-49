42891abccf11c6f40c2ceb056716d7b5eb38a331
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-10 20:00:39 +0500

    feat: add entry point for even number game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+from brain_games.games.even import get_even_task
+from brain_games.user_interaction import run_game
+
+TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def main():
+    run_game(TASK, get_even_task)
+
+
+if __name__ == '__main__':
+    main()
