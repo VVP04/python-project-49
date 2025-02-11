27a9ff4d1323bbd8ad95a7c559b95f92fb932b40
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-11 17:09:55 +0500

    feat: add entry point for brain-calc game

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+from brain_games.games.calc import get_calc_task
+from brain_games.user_interaction import run_game
+
+TASK = 'What is the result of the expression?'
+
+
+def main():
+    run_game(TASK, get_calc_task)
+
+
+if __name__ == '__main__':
+    main()
