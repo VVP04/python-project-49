00921a5ca3c223faf1decf49832c58a8764356a8
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-12 22:20:21 +0500

    feat: add entry point for brain-progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+from brain_games.games.progression import get_progression_task
+from brain_games.user_interaction import run_game
+
+TASK = 'What number is missing in the progression?'
+
+
+def main():
+    run_game(TASK, get_progression_task)
+
+
+if __name__ == '__main__':
+    main()
