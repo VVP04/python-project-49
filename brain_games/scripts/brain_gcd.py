3cff9f8ddd96ee36edb4a44860a5f5a069fcd4ae
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-12 10:36:27 +0500

    feat: add entry point for brain-gcd game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+from brain_games.games.gcd import get_gcd_task
+from brain_games.user_interaction import run_game
+
+TASK = 'Find the greatest common divisor of given numbers.'
+
+
+def main():
+    run_game(TASK, get_gcd_task)
+
+
+if __name__ == '__main__':
+    main()
