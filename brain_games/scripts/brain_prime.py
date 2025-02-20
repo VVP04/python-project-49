e3d69b4850aef7b0ba37e8bf3b299ebf73ad0338
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-20 12:56:48 +0500

    refactor: rework game launch logic in brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.consts import PRIME_INSTUCTION
-from brain_games.engine import run_game
-from brain_games.games.prime import get_prime_task
+from brain_games.games.prime import run_prime_game
 
 
 def main():
-    run_game(PRIME_INSTUCTION, get_prime_task)
+    run_prime_game()
 
 
 if __name__ == '__main__':
