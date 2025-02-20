9868631d9b9a2563424f91531cc20f3c48583ef9
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-20 13:05:36 +0500

    refactor: improve game logic and function names in even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,12 +1,16 @@
-import random
+from brain_games.consts import EVEN_INSTUCTION
+from brain_games.engine import run_game
+from brain_games.utility import get_random_number
 
 
-def check_for_parity(num: int) -> str:
-    if num % 2 == 0:
-        return 'yes'
-    return 'no'
+def is_even(num: int) -> bool:
+    return num % 2 == 0
 
 
-def get_even_task():
-    random_number = random.randint(1, 100)
-    return (random_number, check_for_parity(random_number))
+def get_num_and_even_check() -> tuple:
+    num = get_random_number()
+    return (num, 'yes' if is_even(num) else 'no')
+
+
+def run_even_game():
+    return run_game(EVEN_INSTUCTION, get_num_and_even_check)
