cfee7ad38897f305038e113fe3a8e5c50df0b2ec
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-20 13:06:22 +0500

    refactor: improve game logic and function names in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,19 +1,31 @@
-import random
+from brain_games.consts import (
+    MAX_PROGRESSION_LENGHT,
+    MIN_PROGRESSION_LENGHT,
+    PROGRESSION_INSTUCTION,
+)
+from brain_games.engine import run_game
+from brain_games.utility import get_random_number
 
 
-def generate_progression(start: int, difference: int) -> list:
+def generate_progression() -> list:
     progression = []
-    while len(progression) != 10:
-        progression.append(start)
-        start = start + difference
-    return progression
+    start_progression = get_random_number()
+    difference = get_random_number(0, 30)
+    len_progression = get_random_number(MIN_PROGRESSION_LENGHT, 
+                                        MAX_PROGRESSION_LENGHT)
+    for _ in range(len_progression):
+        progression.append(start_progression)
+        start_progression = start_progression + difference
+    return (progression, len_progression)
 
 
-def get_progression_task():
-    start = random.randint(1, 100)
-    difference = random.randint(1, 30)
-    progression = generate_progression(start, difference)
-    random_index = random.randint(0, 9)
-    correct_answer = str(progression[random_index])
+def get_progression_and_miss_num():
+    progression, len_progression = generate_progression()
+    random_index = get_random_number(0, len_progression - 1)
+    miss_num = progression[random_index]
     progression[random_index] = '..'
-    return (' '.join([str(x) for x in progression]), correct_answer)
+    return (' '.join([str(x) for x in progression]), str(miss_num))
+
+
+def run_progression_game():
+    return run_game(PROGRESSION_INSTUCTION, get_progression_and_miss_num)
