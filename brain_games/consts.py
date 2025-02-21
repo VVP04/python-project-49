377196c478e829516fd16a312ca04649acf22c52
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-21 14:16:19 +0500

    refactor(consts): remove unused MATH_OPERATORS

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -4,6 +4,5 @@ GCD_INSTUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRESSION_INSTUCTION = 'What number is missing in the progression?'
 PRIME_INSTUCTION = 'Answer "yes" if given number is prime. ' \
     'Otherwise answer "no".'
-MATH_OPERATORS = ('+', '-', '*')
 MIN_PROGRESSION_LENGHT, MAX_PROGRESSION_LENGHT = 5, 10
 NUMBER_OF_ROUNDS = 3
