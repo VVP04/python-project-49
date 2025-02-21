a148c641459de28308cf1f9387f357bbcc3b0259
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-21 14:18:50 +0500

    refactor(calc): replace eval with safe operation functions

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,20 @@
 from random import choice
 
-from brain_games.consts import CALC_INSTUCTION, MATH_OPERATORS
+from brain_games.consts import CALC_INSTUCTION
 from brain_games.engine import run_game
 from brain_games.utility import get_random_number
 
 
 def get_numeric_expression_and_result() -> tuple:
     num1, num2 = get_random_number(), get_random_number()
-    math_operator = choice(MATH_OPERATORS)
+    operations = [
+        ('+', lambda x, y: x + y),
+        ('-', lambda x, y: x - y),
+        ('*', lambda x, y: x * y)
+    ]
+    math_operator, operation = choice(operations)
     numeric_expression = f'{num1} {math_operator} {num2}'
-    result = eval(numeric_expression)
+    result = operation(num1, num2)
     return (numeric_expression, str(result))
 
 
