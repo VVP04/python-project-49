1aed4ff9e5358d28be034b93fe8fb6e57591cd2f
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-21 14:32:34 +0500

    refactor(gcd): use math.gcd and store results in variables

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,18 +1,15 @@
+from math import gcd
+
 from brain_games.consts import GCD_INSTUCTION
 from brain_games.engine import run_game
 from brain_games.utility import get_random_number
 
 
-def gcd(num1: int, num2: int) -> int:
-    while num2:
-        num1, num2 = num2, num1 % num2
-    return num1
-
-
 def get_numbers_and_gcd() -> tuple:
     num1, num2 = get_random_number(start=1), get_random_number(start=1)
     gcd_pair = f"{num1} {num2}"
-    return (gcd_pair, str(gcd(num1, num2)))
+    result_of_gcd = str(gcd(num1, num2))
+    return (gcd_pair, result_of_gcd)
 
 
 def run_gcd_game():
