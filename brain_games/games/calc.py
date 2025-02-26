055aab15aa6d1ffcb003e8247e4e75b78c00b14f
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-26 17:48:49 +0500

    refactor: split logic in calc.py into separate functions

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,14 +5,17 @@ from brain_games.engine import run_game
 from brain_games.utility import get_random_number
 
 
-def get_numeric_expression_and_result() -> tuple:
-    num1, num2 = get_random_number(), get_random_number()
-    operations = [
+def get_operations():
+    return [
         ('+', lambda x, y: x + y),
         ('-', lambda x, y: x - y),
         ('*', lambda x, y: x * y)
     ]
-    math_operator, operation = choice(operations)
+
+
+def get_numeric_expression_and_result() -> tuple:
+    num1, num2 = get_random_number(), get_random_number()
+    math_operator, operation = choice(get_operations())
     numeric_expression = f'{num1} {math_operator} {num2}'
     result = operation(num1, num2)
     return (numeric_expression, str(result))
