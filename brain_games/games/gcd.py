5808905b23cb1982b40cc12f8322b866bb338f92
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-26 17:22:38 +0500

    refactor: move str() conversion to return statement in gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,8 +8,8 @@ from brain_games.utility import get_random_number
 def get_numbers_and_gcd() -> tuple:
     num1, num2 = get_random_number(start=1), get_random_number(start=1)
     gcd_pair = f"{num1} {num2}"
-    result_of_gcd = str(gcd(num1, num2))
-    return (gcd_pair, result_of_gcd)
+    result_of_gcd = gcd(num1, num2)
+    return (gcd_pair, str(result_of_gcd))
 
 
 def run_gcd_game():
