8ca5dfd0bfe62633a671606b342770fca34d99ec
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-02-27 17:06:53 +0500

    refactor: improve calc.py readability and modularity

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,17 +5,18 @@ from brain_games.engine import run_game
 from brain_games.utility import get_random_number
 
 
-def get_operations():
-    return [
-        ('+', lambda x, y: x + y),
-        ('-', lambda x, y: x - y),
-        ('*', lambda x, y: x * y)
-    ]
+def get_random_math_operator_and_operation():
+    math_operator_and_operation = choice([
+        ('+', lambda num1, num2: num1 + num2),
+        ('-', lambda num1, num2: num1 - num2),
+        ('*', lambda num1, num2: num1 * num2)
+    ])
+    return math_operator_and_operation
 
 
 def get_numeric_expression_and_result() -> tuple:
     num1, num2 = get_random_number(), get_random_number()
-    math_operator, operation = choice(get_operations())
+    math_operator, operation = get_random_math_operator_and_operation()
     numeric_expression = f'{num1} {math_operator} {num2}'
     result = operation(num1, num2)
     return (numeric_expression, str(result))
